05b946ca94b80224b317f1173e061dacb3052bc6
mimao <qiaoxiaoxi621@gmail.com>
2022-09-06 16:51:31 +0800

    release 1.0.0-dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
             signAllPublications()
             pom {
                 group = "io.github.joesteven"
-                version = "1.0.0"
+                version = "1.0.0-dev"
                 name.set("kmp-walletconnect")
                 description.set("Video player for Kotlin multiplatform")
                 url.set("https://github.com/JoeSteven/KMP-VideoPlayer/")
